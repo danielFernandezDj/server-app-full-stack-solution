f5b2ec9c1df2b989e638eef76a309be6b7e6f51b
Daniel Fernandez <mdanif9728@gmail.com>
2024-09-06 10:12:35 -0700

    preparing the server.js to run in the first root

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")))
+
+// Allow CORS too the client | Frontend.
+// const corsOptions = {
+//   origin: 'https://backend-production-1cdd.up.railway.app/',
+//   optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
+app.use(cors());
+
+// dotenv setup
+require('dotenv').config()
+
+// Roots
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Controllers
+// N/A for now!
+
+const port = process.env.PORT || 1010
+app.listen(port, () => {
+  console.log(`🎣 Fishing on port: ${port}`)
+})
